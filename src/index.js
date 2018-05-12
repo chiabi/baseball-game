41a61eca25e8da17671457e4183889161fe0043a
chiabi <chiabi88@gmail.com>
2018-05-13 02:19:57 +0900

    클래스로 로직 옮김, 클래스 필드가 오류로 인식하는 부분 확인필요함

--- src/index.js
+++ src/index.js
@@ -1,65 +1,55 @@
-// 유효한 난수(1 - 9)
-// 일치하지 않는 유효한 난수 3개의 배열을 구한다.
-
-// 테스트용
-// let i = 100;
-// while (i--) {
-//   console.log(getRandomArray());
-// }
-// const a = new Date;
-// const b = new Date;
-// console.log(Math.abs(a.getMilliseconds() - b.getMilliseconds()));
-
-// 난수 배열 첫번째 방법: do whil, randoms.length
-function getRandomArray() {
-  const randoms = [];
-  do {
-    const random = Math.ceil(Math.random() * 9);
-    if (!randoms.includes(random)) randoms.push(random);
-  } while(randoms.length < 3);
-  return randoms;
-}
-
-
-// 난수 배열 두번째 방법: for문
-function getRandomArray() {
-  const randoms = [null, null, null];
-  for (let i = 0; i < 3; ) {
-    const random = Math.ceil(Math.random() * 9);
-    if (!randoms.includes(random)) {
-      randoms[i] = random;
-      i++;
-    } 
+class BaseballGameLogic {
+  // 왜 인지 모르겠지만 parcel이 클래스 필드 오류라고 인식함 일단 생성자로 처리함
+  // 15퍼즐에서는 잘만 되더니...빌드에서 문제가 생기나...
+  constructor() {
+    this.randomStrike = [];
   }
-  return randoms;
-}
-
-// 별로 성능 차이 안나는 듯
-const randomStrike = getRandomArray();
-
-// 배열을 받아 구해놓은 랜덤 수랑 자리가 얼마나 일치하는 지 문자열로 반환하는 함수
-function checkArray(arr) {
-  // 볼
-  let includeItems = 0;
-  //  스트라이크
-  let equalItems = 0;
-  for(let i = 0; i < 3; i++) {
-    if(parseInt(arr[i]) === randomStrike[i]) {
-      equalItems++;
-    } else if (randomStrike.includes(parseInt(arr[i]))) {
-      includeItems++;
+  init() {
+    this.getRandomArray();
+  }
+  // 3개의 겹치지 않는 랜덤 수를 담은 배열을 반환한다.
+  getRandomArray() {
+    const randoms = [null, null, null];
+    for (let i = 0; i < 3; ) {
+      const random = Math.ceil(Math.random() * 9);
+      if (!randoms.includes(random)) {
+        randoms[i] = random;
+        i++;
+      } 
     }
+    return randoms;
   }
-  return equalItems === 0 && includeItems === 0 ? `OUT` : `${equalItems} 스트라이크 : ${includeItems} 볼`; 
+  // 배열을 받아 구해놓은 랜덤 수랑 자리가 얼마나 일치하는 지 객체를 반환하는 함수 
+  // 일치하는 수가 없으면 null을 반환
+  checkArray(arr) {
+    // 볼
+    let includeItems = 0;
+    //  스트라이크
+    let equalItems = 0;
+    for(let i = 0; i < 3; i++) {
+      if(parseInt(arr[i]) === randomStrike[i]) {
+        equalItems++;
+      } else if (randomStrike.includes(parseInt(arr[i]))) {
+        includeItems++;
+      }
+    }
+    return equalItems === 0 && includeItems === 0 ? null : {equal: equalItmes, include: includeItems}; 
+  }
 }
+const game = new BaseballGameLogic();
+game.init();
 
 const pitchDigitsArray = [null, null, null];
-
 const pitch = document.querySelector('.pitch');
 const pitchDigits = pitch.querySelectorAll('.pitch-input__digit');
-const pitchMessage = pitch.querySelector('.pitch-input__message');
-console.log(pitch, pitchDigits);
+const pitchMessage = pitch.querySelector('.pitch-message');
+const buttonPitch = pitch.querySelector('.pitch-input__btn-pitch');
+const buttonRestart = pitch.querySelector('.pitch-input__btn-restart');
+
+// console.log(pitch, pitchDigits);
+// 숫자인지 확인하기 위해
 const reg = new RegExp(/[1-9]/);
+
 // 첫 인풋에 포커스를 넣어준다.
 pitchDigits[0].focus();
 pitchDigits.forEach((item, index, arr) => {
@@ -81,4 +71,12 @@ pitchDigits.forEach((item, index, arr) => {
     console.log(pitchDigitsArray, inputValue,  pitchDigitsArray.includes(inputValue))
     // 다음 입력으로 포커스 맞추기
   });
+});
+
+// pitch 버튼이 눌리면, pitchDigitsArray 값이 유효한 지 확인, 
+// 유효하지 않으면 동작하지 않음
+// pitchDigitsArray의 배열을 체크해서 생성된 난수 배열과 맞는지 확인
+// pitchDigitsArray 배열의 값을 초기화(null로)
+buttonPitch.addEventListener('click', e => {
+
 });
