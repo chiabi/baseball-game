a0ab5cc3328041db9a6ee924b9796d146fcfb3a3
chiabi <chiabi88@gmail.com>
2018-05-14 00:07:13 +0900

    디자인 스타일 추가, 기존 코드 오류 수정, 결과 리스트에 추가되는 부분 추가, 9회에 게임 종료되는 부분 추가 필요

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 class BaseballGameLogic {
-  // 왜 인지 모르겠지만 parcel이 클래스 필드 오류라고 인식함 일단 생성자로 처리함
-  // 15퍼즐에서는 잘만 되더니...빌드에서 문제가 생기나...
-  constructor() {
-    this.randomStrike = [];
-  }
+  randomStrike = [];
+  // a = 1;
   init() {
-    this.getRandomArray();
+    this.randomStrike = this.getRandomArray();
   }
   // 3개의 겹치지 않는 랜덤 수를 담은 배열을 반환한다.
   getRandomArray() {
@@ -22,22 +19,25 @@ class BaseballGameLogic {
   // 배열을 받아 구해놓은 랜덤 수랑 자리가 얼마나 일치하는 지 객체를 반환하는 함수 
   // 일치하는 수가 없으면 null을 반환
   checkArray(arr) {
-    // 볼
-    let includeItems = 0;
-    //  스트라이크
-    let equalItems = 0;
+    const result = {
+      strike: 0,
+      ball: 0
+    }
     for(let i = 0; i < 3; i++) {
-      if(parseInt(arr[i]) === randomStrike[i]) {
-        equalItems++;
-      } else if (randomStrike.includes(parseInt(arr[i]))) {
-        includeItems++;
+      if(parseInt(arr[i]) === this.randomStrike[i]) {
+        result.strike++;
+      } else if (this.randomStrike.includes(parseInt(arr[i]))) {
+        result.ball++;
       }
     }
-    return equalItems === 0 && includeItems === 0 ? null : {equal: equalItmes, include: includeItems}; 
+    console.log(result);
+    return result; 
   }
 }
+
 const game = new BaseballGameLogic();
 game.init();
+console.log(game.randomStrike);
 
 const pitchDigitsArray = [null, null, null];
 const pitch = document.querySelector('.pitch');
@@ -45,6 +45,7 @@ const pitchDigits = pitch.querySelectorAll('.pitch-input__digit');
 const pitchMessage = pitch.querySelector('.pitch-message');
 const buttonPitch = pitch.querySelector('.pitch-input__btn-pitch');
 const buttonRestart = pitch.querySelector('.pitch-input__btn-restart');
+const gameResultList = document.querySelector('.game-result__list');
 
 // console.log(pitch, pitchDigits);
 // 숫자인지 확인하기 위해
@@ -56,19 +57,19 @@ pitchDigits.forEach((item, index, arr) => {
   item.addEventListener('keyup', e => {
     // 입력은 한 글자만 받고 숫자만 입력받는다.
     const inputValue = item.value;
-    if (inputValue != null && pitchDigitsArray.includes(inputValue)) {
+    if (inputValue != null && pitchDigitsArray.indexOf(inputValue) !== -1 && pitchDigitsArray.indexOf(inputValue) !== index) {
       pitchMessage.textContent = `중복되는 값입니다.`;
     } else if (inputValue != null && (reg.test(inputValue) && inputValue.length === 1)) {
       pitchDigitsArray[index] = inputValue; 
       item.nextElementSibling.focus();
       pitchMessage.textContent = '';
-      console.log(item.value);
+      // console.log(item.value);
     } else if (inputValue) {
       pitchMessage.textContent = `유효한 값이 아닙니다.`;
     } else {
       pitchMessage.textContent = '';
     }
-    console.log(pitchDigitsArray, inputValue,  pitchDigitsArray.includes(inputValue))
+    // console.log(pitchDigitsArray, inputValue,  pitchDigitsArray.includes(inputValue))
     // 다음 입력으로 포커스 맞추기
   });
 });
@@ -78,5 +79,33 @@ pitchDigits.forEach((item, index, arr) => {
 // pitchDigitsArray의 배열을 체크해서 생성된 난수 배열과 맞는지 확인
 // pitchDigitsArray 배열의 값을 초기화(null로)
 buttonPitch.addEventListener('click', e => {
-
+  // console.log(pitchDigitsArray);
+  if(pitchDigitsArray.some(item => null || !reg.test(item))) {
+    return false;
+  }
+  gameResultList.appendChild(render(pitchDigitsArray));
+  // 초기화
+  for (let i = 0; i < 3; i++) {
+    pitchDigitsArray[i] = null;
+    pitchDigits[i].value = null;
+    console.log('boo');
+  }
+  pitchDigits[0].focus();
+  // console.log(pitchDigitsArray);
 });
+
+function render(arr) {
+  const {strike, ball} = game.checkArray(arr);
+  const item = document.createElement('li');
+  for (let i = 0; i < 3; i++) {
+    const el = document.createElement('span');
+    el.classList.add('digit');
+    el.textContent = arr[i];
+    item.appendChild(el);
+  }
+  const txtEl = document.createElement('div');
+  txtEl.classList.add('txt');
+  txtEl.innerHTML = strike === 0 && ball === 0 ? `<em>OUT</em>` : `<em>${strike}</em> Strike <em>${ball}</em> Ball`;
+  item.appendChild(txtEl);
+  return item;
+}
